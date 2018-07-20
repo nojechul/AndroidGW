949ce9a9c2ed7a610bbb1f381cb54e5b7742801d
NightNMare <dlarbdnjs1@naver.com>
2018-07-21 00:35:49 +0900

    little

--- app/src/main/java/com/example/sunrinton/interpeople/SampleGattAttributes.java
+++ app/src/main/java/com/example/sunrinton/interpeople/SampleGattAttributes.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
  */
 public class SampleGattAttributes {
     private static HashMap<String, String> attributes = new HashMap();
-    public static String HM_10 = "0000ffe1-0000-1000-8000-00805f9b34fb";
+    public static String HM_10 = "0000ffe0-0000-1000-8000-00805f9b34fb";
     public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
 
     static {
